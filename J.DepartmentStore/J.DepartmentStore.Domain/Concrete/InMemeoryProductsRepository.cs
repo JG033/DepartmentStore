6dd8a22194e2f7390d24d9f9735efca7194bd6be
JG <JG>
2020-07-30 18:29:37 -0400

    [CHG]add product page

---
+++ J.DepartmentStore/J.DepartmentStore.Domain/Concrete/InMemeoryProductsRepository.cs
@@ -0,0 +1,27 @@
+﻿using J.DepartmentStore.Domain.Abstract;
+using J.DepartmentStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J.DepartmentStore.Domain.Concrete
+{
+    public class InMemeoryProductsRepository : IProductsRepository
+    {
+        private List<Product> _products = new List<Product>
+        {
+            new Product {Name = "Glasses", Price = 99},
+            new Product {Name = "Watches", Price = 199},
+            new Product {Name = "Handbag", Price = 299},
+            new Product {Name = "Furniture", Price = 599},
+            new Product {Name = "Clothes", Price = 59}
+        };
+
+        public IEnumerable<Product> Products
+        {
+            get { return _products; }
+        }
+    }
+}
---
+++ J.DepartmentStore/J.DepartmentStore.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using J.DepartmentStore.Domain.Abstract;
+using J.DepartmentStore.Domain.Concrete;
+using J.DepartmentStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace J.DepartmentStore.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IProductsRepository ProductsRepository { get; set; }
+        = new InMemeoryProductsRepository();
+        // GET: Product
+
+        public ViewResult List()
+        {
+            var model = ProductsRepository.Products;
+            return View(model);
+        }
+        
+    }
+}
